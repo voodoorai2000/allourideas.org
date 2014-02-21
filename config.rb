46ff9ac4f6be19e5fe604c094f314f536317eac6
Miguel Angel Davila <miguelangel@fkla.org>
2014-02-21 14:58:08 +0100

    setting bootstrap as css framework

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-# Require any additional compass plugins here.
-require 'bootstrap-sass'
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/photocracy"
